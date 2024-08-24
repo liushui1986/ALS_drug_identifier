a9faf387b316124449944539ea021456b4114d28
liushui1986 <liushui1986@SHUAIPENG.localdomain>
2024-08-24 14:55:02 -0500

    make a new folder for the deep learning file

--- 07302024_version_test3/02_flaskapp/run.py
+++ 07302024_version_test3/02_flaskapp/run.py
@@ -112,4 +112,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5001)
